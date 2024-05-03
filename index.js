4b1b4380e925cc9b2b1032ef821b7ac173e002a6
Daniela Leal <falfada@gmail.com>
2024-05-03 17:33:47 +1000

    Added questions to index.js

--- index.js
+++ index.js
@@ -1,62 +1,82 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const generateMarkdown = require('./utils/generateMarkdown');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        message: "Write your github username",
+        name: 'githubUsername'
+    },
+    {
+        type: 'input',
+        message: "Write your email address",
+        name: 'email'
+    },
+    {
+        type: 'input',
+        message: 'Write the title of your project',
+        name: 'title'
+    },
+    {
+        type: 'input',
+        message: 'Write the description:',
+        name: 'description'
+    },
+    {
+        type: 'input',
+        message: "What are the steps required to install your project?",
+        name: 'installation'
+    },
+    {
+       type: 'input' ,
+       message: "Provide instructions for use",
+       name: 'usage'
+    },
+    {
+        type: 'confirm',
+        message: "Do you wish to use the default contribution guidelines?",
+        name: 'contribution',
+        default: 'false',
+    },
+    {
+        type: 'confirm',
+        message: "Would you like to use the default test instructions?",
+        name: 'tests',
+        default: 'false'
+    },
+    {
+        type: 'list',
+        message: "What license do you want to use? Choose from the list",
+        name: 'license',
+        choices: ["MIT License", "Apache LIcense 2.0", "GNU GPLv.3.0"],
+    }
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
-
-// TODO: Create a function to initialize app
-function init() {}
-
-// Function call to initialize app
-init();
-
-
-const inquirer = require('inquirer');
 
 function handleResponse(response){
     console.log(JSON.stringify(response));
 }
 
-function askTitle(){
-    return inquirer.prompt([
-        {
-            type: 'input',
-            message: 'Whats your title?',
-            name: 'title'
-        },
-        {
-            type: 'input',
-            message: 'Write your description:',
-            name: 'description',
-        },
-    ]);
+// TODO: Create a function to initialize app
+function init() {
+    return inquirer.prompt(questions).then(handleResponse);
 }
-function askUser(){
-    const steps = [];
-    
-    inquirer.prompt([
-        {
-            type: 'input',
-            message: 'Provide the step description of how to get the development environment running.',
-            name: 'step',
-        },
-        {
-            type: 'confirm',
-            message: 'Do you want to add another step?',
-            name: 'addAnother',
-            default: 'false',
-        },
-    ]);
-}
-async function callInquirers(){
-    const inq1 = await askTitle();
-    const inq2 = await askUser();
-    return{
-        title: inq1.title,
-        description: inq1.description,
-        step: inq2.step,
-    }
-}
-callInquirers().then(handleResponse);
+
+// Function call to initialize app
+init();
+
+
+// async function callInquirers(){
+//     const inq1 = await askTitle();
+//     const inq2 = await askUser();
+//     return{
+//         title: inq1.title,
+//         description: inq1.description,
+//         step: inq2.step,
+//     }
+// }
+// callInquirers().then(handleResponse);
